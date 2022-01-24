424b838b1b0f29866e47a3a5d10094a23108b531
nassim <nassim@nassimfatmi.localdomain>
2022-01-23 17:29:23 -0800

    Créer une réservation fonctionnalité

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -55,6 +55,23 @@ public class UserController {
 		return "home";
 
 	}
+
+	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+		
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		reserveService.reserve(reserve);
+	
+		
+		return "home";
+
+	}
 }
 
 
